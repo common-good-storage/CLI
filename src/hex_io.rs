d9be4430cecb6851bdf1c705a0878b081e0296e6
Joonas Koivunen <joonas.koivunen@gmail.com>
2021-04-01 14:29:59 +0300

    refactor: remove added conversions

--- src/hex_io.rs
+++ src/hex_io.rs
@@ -118,32 +118,6 @@ impl FromStr for AnyKey {
     }
 }
 
-impl AnyKey {
-    #[allow(unused)]
-    pub(crate) fn as_sr25519(&self) -> Option<&[u8; 32]> {
-        match self {
-            AnyKey::Sr25519(x) => Some(x),
-            _ => None,
-        }
-    }
-
-    #[allow(unused)]
-    pub(crate) fn as_bls_private(&self) -> Option<&[u8; 32]> {
-        match self {
-            AnyKey::BlsPrivate(x) => Some(x),
-            _ => None,
-        }
-    }
-
-    #[allow(unused)]
-    pub(crate) fn as_bls_public(&self) -> Option<&[u8; 48]> {
-        match self {
-            AnyKey::BlsPublic(x) => Some(x),
-            _ => None,
-        }
-    }
-}
-
 impl fmt::Debug for AnyKey {
     fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
